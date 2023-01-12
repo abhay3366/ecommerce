310d59071ca250b6eb473c4adeffce2faa93d81e
Abhay Kant Tiwari <tabhaykant2@gmail.com>
2023-01-13 03:44:05 +0530

    add sanity data

---
+++ ecommerce1/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'uwbl3d8v',
+    dataset: 'production'
+  }
+})
---
+++ ecommerce1/schemas/product.js
@@ -0,0 +1,9 @@
+export default{
+    name:"prdoduct",
+    title:"Product",
+    type:"document",
+    fields:{
+        name:"image",
+        title
+    }
+}
